5b03be03a2f62ca76f316c8d334d8e8a7b3a46e1
Gabrielle <31347690+gabartomeo@users.noreply.github.com>
2020-06-13 10:49:18 -0400

    Update basic_diceroller.py
    Simpler roll, now gives the list of rolls as well

--- basic_diceroller.py
+++ basic_diceroller.py
@@ -16,7 +16,7 @@ main_page = """
 </html>
 """
 
-main_page2 = re.sub("(Result:)", "\\1 {{ result }}", main_page)
+main_page2 = re.sub("(Result:)", "\\1 {{ result }} {{ rolls }}", main_page)
 
 @app.route("/", methods=["GET"])
 def index():
@@ -29,16 +29,14 @@ def diceRoller():
 		die = re.findall("((\d+)d(\d+))", text)
 		if die == []:
 			die = [("", 1, 20)]
-		roll_sum = roll(int(die[0][1]), int(die[0][2]))
-	templateData = {"result": roll_sum}
+		user_rolls = roll(int(die[0][1]), int(die[0][2]))
+	templateData = {"result": user_rolls["sum"], "rolls": user_rolls["list"]}
 	return render_template_string(main_page2, **templateData)
 
-def roll(num, sides, roll_sum=0):
-	roll_sum += random.randint(1, sides)
-	if num > 1:
-		return(roll(num-1, sides, roll_sum))
-	else:
-		return(roll_sum)
+def roll(num, sides):
+	rolls = [random.randint(1, sides) for i in range(num)]
+	roll_sum = sum(rolls)
+	return({"list": rolls, "sum": roll_sum})
 
 if __name__ == '__main__':
     app.run()
